c441642b0b00b12fe0c5ec08e47f1c922c8d9fae
Tan Shuwen <tsw822@gmail.com>
2020-12-03 11:30:01 -0500

    add nav_index version 1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var Article = require('../model/article')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express using Nodemon' });
+    Article.find({}).exec(function(err,articles){
+      // console.log(err);
+      res.render('index',{
+        articles:articles,
+        pageTitle: "TD Blog"});
+    });
 });
 
 module.exports = router;
